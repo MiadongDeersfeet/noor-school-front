c336aa0db16fbafabd43a9e3a37523d20910a7c4
Noor Kim <ani.ana.yungi@gmail.com>
2026-04-24 18:47:44 +0900

    ci: add frontend auto deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   plugins: [react()],
   base: "/",
   build: {
-    outDir: "../src/main/resources/static",
+    outDir: "dist",
     emptyOutDir: true,
   },
   server: {
